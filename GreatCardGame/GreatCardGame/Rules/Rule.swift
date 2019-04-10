55928da141e3d47f79ef2355e0127e3dec0e9f57
Christopher Olsen <c.alan.olsen@gmail.com>
2019-04-09 21:57:06 -0500

    add rules from students

--- GreatCardGame/GreatCardGame/Rules/Rule.swift
+++ GreatCardGame/GreatCardGame/Rules/Rule.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+struct RuleChecker {
+
+    var activeRule = LiftRule.self
+
+    func checkCardForPoints(_ card: Card) -> Int {
+        return activeRule.calculatePoints(for: card)
+    }
+}
+
 protocol Rule {
     static func calculatePoints(for card: Card) -> Int
 }
@@ -18,5 +27,33 @@ struct SimpleRule: Rule {
     /// value in points.
     static func calculatePoints(for card: Card) -> Int {
         return card.numericValue
+    }
+}
+
+struct DavonteRule: Rule {
+
+    /// A very simple rule. Each card is worth it's face
+    /// value in points.
+    static func calculatePoints(for card: Card) -> Int {
+        if card.suit?.color == .red {
+            return -13
+        } else {
+            return card.numericValue
+        }
+    }
+}
+
+struct LiftRule: Rule {
+
+    /// A very simple rule. Each card is worth it's face
+    /// value in points.
+    static func calculatePoints(for card: Card) -> Int {
+        if card.numericValue < 10 {
+            return 100
+        } else if card.numericValue > 10 {
+            return card.numericValue
+        } else {
+            return card.numericValue
+        }
     }
 }
